58c1e3213c3dcfa78498bca508113719e0e81a01
meganjar <68120966+meganjar@users.noreply.github.com>
2025-03-12 22:03:55 -0600

    create backend folder structure

---
+++ backend/model/user.model.mjs
@@ -0,0 +1,14 @@
+const user Schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
