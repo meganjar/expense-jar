b1843f2e1c1694073cd687fff55ae07cc30aa4b2
meganjar <68120966+meganjar@users.noreply.github.com>
2025-03-17 18:24:30 -0600

    tailwind 4 congif

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AnalyticsPage from "./pages/AnalyticsPage";
 import HomePage from "./pages/HomePage";
@@ -7,29 +7,26 @@ import LoginPage from "./pages/LoginPage";
 import ReceiptsPage from "./pages/ReceiptsPage";
 import LowerNav from "./Components/Nav";
 
-
 function App() {
   // const res = await fetch("http://localhost:8080/");
   // const data = await res.json();
   // console.log(data)
 
-  
-    async function handleSubmit(e) {}
+  async function handleSubmit(e) {}
 
-
-    return (
+  return (
     <>
-      <LowerNav />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/receipts" element={<ReceiptsPage />} />
-        <Route path="/analytics" element={<AnalyticsPage />} />
-      </Routes>
-        <h1>ummm hello</h1>
+      <div className="bg-black min-h-screen">
+        <LowerNav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/receipts" element={<ReceiptsPage />} />
+          <Route path="/analytics" element={<AnalyticsPage />} />
+        </Routes>
         <button onClick={handleSubmit}>Submit</button>
+      </div>
     </>
-    )
-  
+  );
 }
 export default App;
--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -25,6 +25,7 @@ function LowerNav() {
           />
         </ul>
       </nav>
+      hihi
     </div>
   );
 }
