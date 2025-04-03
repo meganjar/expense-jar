bf9f8cf041cee4f51f0e62e9c91558a684ec3bbc
meganjar <68120966+meganjar@users.noreply.github.com>
2025-04-03 15:16:27 -0600

    add classic signup component to front

---
+++ frontend/src/Components/TradSignup.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class TradSignup extends Component {
+  render() {
+    return (
+      <div>TradSignup</div>
+    )
+  }
+}
+
+export default TradSignup
