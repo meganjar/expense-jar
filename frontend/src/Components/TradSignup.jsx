8094ea43bd025b48ac41d2924963ec6c0c569e9d
meganjar <68120966+meganjar@users.noreply.github.com>
2025-04-03 15:21:34 -0600

    add attributes to input elements on signup comp

--- frontend/src/Components/TradSignup.jsx
+++ frontend/src/Components/TradSignup.jsx
@@ -4,7 +4,7 @@ export class TradSignup extends Component {
   render() {
     return (
       <div>TradSignup
-        <label htmlFor='date'><Email></Email></label>
+        <label htmlFor='date'>email</label>
       <input
         type='string'
         
@@ -12,12 +12,11 @@ export class TradSignup extends Component {
         name='transactionDate'
         required
       />
-      <label htmlFor='date'><Email></Email></label>
+      <label htmlFor='date'>email</label>
       <input
         type='string'
-        
-        id='email-signup'
-        name='transactionDate'
+        id='pwd-signup'
+        name='choose-password'
         required
       />
    
