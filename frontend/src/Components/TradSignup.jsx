8965a471a27181f3ff19ccb5688573f95c8e9dbd
meganjar <68120966+meganjar@users.noreply.github.com>
2025-04-03 15:19:46 -0600

    add labels annd input fields to JSX on signup component

--- frontend/src/Components/TradSignup.jsx
+++ frontend/src/Components/TradSignup.jsx
@@ -3,7 +3,25 @@ import React, { Component } from 'react'
 export class TradSignup extends Component {
   render() {
     return (
-      <div>TradSignup</div>
+      <div>TradSignup
+        <label htmlFor='date'><Email></Email></label>
+      <input
+        type='string'
+        
+        id='email-signup'
+        name='transactionDate'
+        required
+      />
+      <label htmlFor='date'><Email></Email></label>
+      <input
+        type='string'
+        
+        id='email-signup'
+        name='transactionDate'
+        required
+      />
+   
+      </div>
     )
   }
 }
